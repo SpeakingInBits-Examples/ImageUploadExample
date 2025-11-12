6fce6238b1f273a28f91f292b1218c0f7ff75a8a
joeprogrammer88 <joe@speakinginbits.com>
2025-11-12 13:35:56 -0800

    Create a product class

---
+++ ImageUploadExample/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ImageUploadExample.Models;
+
+public class Product
+{
+    [Key]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// User facing display name
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// The URL of the product image
+    /// </summary>
+    public required string ImageUrl { get; set; }
+}
