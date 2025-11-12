db42c69a76912849d7505193ede19d3598f2e816
joeprogrammer88 <joe@speakinginbits.com>
2025-11-12 13:31:55 -0800

    Add project files.

---
+++ ImageUploadExample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ImageUploadExample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
