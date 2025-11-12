ee2a4a5bc3f8c495105baeed8a373d2b582fde34
joeprogrammer88 <joe@speakinginbits.com>
2025-11-12 13:37:37 -0800

    Scaffolded Products controller and views

--- ImageUploadExample/Data/ApplicationDbContext.cs
+++ ImageUploadExample/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ImageUploadExample.Models;
 
 namespace ImageUploadExample.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
+        public DbSet<ImageUploadExample.Models.Product> Product { get; set; } = default!;
     }
 }
